bfed93852c0773d0c64cf7e8ba2c2d8266fb6694
Lucas Black <lucasblack@arista.com>
2017-07-31 18:01:28 -0700

    title change

--- conf.lua
+++ conf.lua
@@ -2,5 +2,5 @@ function love.conf(t)
   t.window.width = 640
   t.window.height = 640
   t.window.vsync = false
-  t.window.title = '[Insert Game Title Here] | LD 39'
+  t.window.title = 'Phil'
 end
